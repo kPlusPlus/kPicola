3d0a8dd0245f74380ed7d00e0c8064b6e2e50f40
kPlusPlus <kresimir.ivkovic@gmail.com>
2018-06-19 09:46:03 +0200

    Doctrine add

--- save_json.php
+++ save_json.php
@@ -1,9 +1,8 @@
 <?php
 
 $myFile = "general.json";
-//$fh = fopen($myFile, 'w') or die("can't open file");
 $fh = fopen($myFile, 'a') or die("can't open file");
-$stringData = $_GET["data"];
+$stringData = $_GET["data"] . PHP_EOL;
 fwrite($fh, $stringData);
 fclose($fh)
 
